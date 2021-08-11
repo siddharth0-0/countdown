953c1ccda2430907f08b5d02370f0d00d8b16d96
siddharth0-0 <agrawalsid05@gmail.com>
2021-08-11 19:33:57 +0530

    build-step

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCBBc4f8ubyV3v672FuAVeDYasy1gv1InY",
+    authDomain: "countdown-610c6.firebaseapp.com",
+    projectId: "countdown-610c6",
+    storageBucket: "countdown-610c6.appspot.com",
+    messagingSenderId: "930777721330",
+    appId: "1:930777721330:web:e9650f0adc8a91b6f4e36b",
+    measurementId: "G-X95BEFKVHV"
+  }
 };
